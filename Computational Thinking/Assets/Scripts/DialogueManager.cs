e8b9336d02159490e963b04783aaf01ffe39d1e8
João Seffrin <jessoares@inf.ufpel.edu.br>
2020-07-06 17:30:54 -0300

    Atividade 1 Prototype

---
+++ Computational Thinking/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text DialogueText;
+    private Queue<string> sentences;
+    private float timer;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Start conver " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        DialogueText.text = sentence;
+    }
+   void EndDialogue()
+    {
+        Debug.Log("End of conver");
+    }
+}
---
+++ Computational Thinking/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float Speed;
+    private float Width;
+    private float Height;
+    private float timeCounter = 0;
+    public float x;
+    public float y;
+    public float z;
+    public Transform center;
+    void Start()
+    {
+        Speed = 2f;
+        Width = 2;
+        Height = 2;
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+        z = this.transform.position.z;
+        timeCounter = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime * Speed;
+        x = Mathf.Cos(timeCounter) * Width;
+        y = -0.5f;
+        z = Mathf.Sin(timeCounter) * Height;
+        transform.position = new Vector3(x, y, z) + center.position;
+
+        
+    }
+}
